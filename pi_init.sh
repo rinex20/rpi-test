57cdbab9e953c64bde4d1c433ac6a21a98eb70c7
rinex20 <rinex20@me.com>
2021-07-30 11:15:15 +0800

    Update pi_init.sh

--- pi_init.sh
+++ pi_init.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# add temp measure shell cmd
+if [ -e /usr/local/bin/temp ];then
+  echo "cmd temp exist."
+else
+  echo "/opt/vc/bin/vcgencmd measure_temp" | sudo tee /usr/local/bin/temp >> /dev/null
+fi
 
-echo "/opt/vc/bin/vcgencmd measure_temp" | sudo tee /usr/local/bin/temp >> /dev/null
+
+
 
 
