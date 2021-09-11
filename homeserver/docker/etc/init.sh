406d4722b6a709241c05863e092910d25c26892f
rinex20 <rinex20@me.com>
2021-09-11 11:51:26 +0800

    Create init.sh

---
+++ homeserver/docker/etc/init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# sysctl.conf
+
+# Uncomment the following to stop low-level messages on console
+# kernel.printk = 3 4 1 3
+# min recommend 1 4 1 3
+
+echo "kernel.printk = 1 4 1 3" >> /etc/sysctl.conf
+
