829883bd0859c78baa973d818666bab4d377f788
rinex20 <rinex20@me.com>
2021-09-11 11:50:01 +0800

    Delete network-init.sh

--- homeserver/network-init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-ip link set eth0_op promisc on
-ip link add macvlan link eth0_op type macvlan  mode bridge
-
-ip addr add 192.168.90.16/32 dev macvlan
-ip link set macvlan up
-
-ip route add 192.168.90.4/30 dev macvlan
